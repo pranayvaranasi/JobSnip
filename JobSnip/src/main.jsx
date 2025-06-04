173a543ca085beef0aa9e27f18def36fccfc1920
Pranay Varanasi <142153387+pranayvaranasi@users.noreply.github.com>
2025-06-04 12:33:35 +0000

    Debugged the problem of layout not rendering

--- JobSnip/src/main.jsx
+++ JobSnip/src/main.jsx
@@ -1,12 +1,12 @@
-import  JobSnip from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import JobSnip from './App.jsx';
 import './index.css';
 
-function App() {
-  return (
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <div className="min-h-screen bg-gray-100">
       <JobSnip />
     </div>
-  );
-}
-
-export default App;
+  </React.StrictMode>
+);
